1957ff092647bf15482ba95e3c683fb8225fe7f2
jhbazza <bazza00@gmail.com>
2020-05-01 16:01:39 -0300

    Blink ok

--- 002-BlinkLed/src/main.cpp
+++ 002-BlinkLed/src/main.cpp
@@ -2,10 +2,15 @@
 
 void setup() {
 
-  pinMode()
-  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
 }
